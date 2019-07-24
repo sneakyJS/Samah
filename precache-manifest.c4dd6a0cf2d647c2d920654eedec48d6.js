3cd6c910ac2ee0454d8f447ab7499217888a66da
Ahd <dafuq.ahd@gmail.com>
2019-07-24 21:01:14 +0300

    Updates

---
+++ precache-manifest.c4dd6a0cf2d647c2d920654eedec48d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46163360b4a10c6226d3707afa054bf",
+    "url": "/Samah/index.html"
+  },
+  {
+    "revision": "716546ca8b58c6331edd",
+    "url": "/Samah/static/css/main.ef314c0f.chunk.css"
+  },
+  {
+    "revision": "a21d466aa0a3e7e84b2b",
+    "url": "/Samah/static/js/2.ae6e716f.chunk.js"
+  },
+  {
+    "revision": "716546ca8b58c6331edd",
+    "url": "/Samah/static/js/main.c49398a7.chunk.js"
+  },
+  {
+    "revision": "8fe1d2f08be5983266da",
+    "url": "/Samah/static/js/runtime~main.87c7485d.js"
+  }
+]);
